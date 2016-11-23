cdf87616d19b7651863f040e32236bf2cf4601cc
skypotato <hunsooski@naver.com>
2016-11-23 21:26:54 +0900

    java modle2 구조

---
+++ src/controller/BoardFrontController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action;
+import vo.ActionForward;
+
+@WebServlet("*.bo")
+public class BoardFrontController extends javax.servlet.http.HttpServlet {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		String RequestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = RequestURI.substring(contextPath.length());
+		ActionForward forward = null;
+		Action action = null;
+
+		/*
+		 * if(command.equals("/boardWriteForm.bo")){ forward=new
+		 * ActionForward(); forward.setPath("/board/qna_board_write.jsp"); }else
+		 * if(command.equals("/boardWritePro.bo")){
+		 * 
+		 * }
+		 */
+		if (forward != null) {
+
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+
+		}
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
---
+++ src/db/JdbcUtil.java
@@ -0,0 +1,82 @@
+package db;
+
+import java.sql.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class JdbcUtil {
+
+	/* Connection Pool에서 Connection 객체를 얻어와서 반환하는 메서드 */
+	public static Connection getConnection() {
+		Connection con = null;
+
+		try {
+			/* 톰캣 자체의 컨텍스트를 얻어 오는 부분 */
+			Context initCtx = new InitialContext();
+			/* Resource 정의에 관한 Context를 얻어오는 부분 */
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			/* context.xml에 정의한 DataSource 객체를 얻어 오는 부분 */
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/movieSite");
+			con = ds.getConnection();
+			con.setAutoCommit(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+	public static void close(Connection con) {
+
+		try {
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void close(Statement stmt) {
+
+		try {
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void close(ResultSet rs) {
+
+		try {
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void commit(Connection con) {
+
+		try {
+			con.commit();
+			System.out.println("commit success");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void rollback(Connection con) {
+
+		try {
+			con.rollback();
+			System.out.println("rollback success");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
